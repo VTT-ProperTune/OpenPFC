4765803db6e7a591c8d2e87fe1f240d498d4222c
Jukka Aho <jukka.aho@vtt.fi>
2022-01-26 12:13:28 +0200

    Write results to binary file, first version

--- pfc-heffte.cpp
+++ pfc-heffte.cpp
@@ -120,43 +120,7 @@ void compute_dft(MPI_Comm comm, json settings) {
     std::cout << "Starting simulation\n\n";
   }
 
-  // start iterations
-  for (auto k = 1; k <= N; k++) {
-    if (me == 0) {
-      std::cout << "Iteration " << k << std::endl;
-    }
-    fft.forward(U.data(), C1.data(), workspace.data(), heffte::scale::full);
-    for (size_t i = 0; i < U.size(); i++) {
-      U[i] = pow(U[i], 3.0) - C * U[i];
-    };
-    fft.forward(U.data(), C2.data(), workspace.data(), heffte::scale::full);
-
-    // note the order of the loops corresponding to the default order (0, 1, 2)
-    // order (0, 1, 2) means that the data in dimension 0 is contiguous
-    for (auto i = outbox.low[2]; i <= outbox.high[2]; i++) {
-      for (auto j = outbox.low[1]; j <= outbox.high[1]; j++) {
-        for (auto k = outbox.low[0]; k <= outbox.high[0]; k++) {
-          // triple indexes to linear index conversion
-          auto idx = (i - outbox.low[2]) * local_plane +
-                     (j - outbox.low[1]) * local_stride + k - outbox.low[0];
-          auto c1 = cos(2 * pi() / nx * i);
-          auto c2 = cos(2 * pi() / ny * j);
-          auto c3 = cos(2 * pi() / nz * k);
-          auto f = 2.0 / pow(h, 2.0) * (c1 + c2 + c3 - 3);
-          auto d = 1.0 - tau * f * (pow(f + 1.0, 2.0) + C - delta);
-          C1[idx] = (C1[idx] + tau * f * C2[i]) / d;
-        }
-      }
-    }
-
-    fft.backward(C1.data(), U.data(), workspace.data(), heffte::scale::full);
-    /*
-    auto N = nx * ny * nz;
-    for (int i = 0; i < U.size(); i++) {
-      U[i] = U[i] / N;
-    }
-    */
-
+  {
     MPI_Datatype filetype;
     const int gdims[] = {nx, ny, nz};
     const auto lx = inbox.high[0] - inbox.low[0];
@@ -173,8 +137,68 @@ void compute_dft(MPI_Comm comm, json settings) {
     const unsigned int disp = 0;
 
     MPI_File fh;
-    const std::string filename =
-        std::to_string(k) + "_" + std::to_string(me) + ".bin";
+    const std::string filename = "0.bin";
+    MPI_File_open(MPI_COMM_WORLD, filename.c_str(),
+                  MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+    MPI_Offset filesize = 0;
+    MPI_File_set_size(fh, filesize); // force overwriting existing data
+    MPI_File_set_view(fh, disp, MPI_DOUBLE, filetype, "native", MPI_INFO_NULL);
+    MPI_File_write_all(fh, U.data(), U.size(), MPI_DOUBLE, MPI_STATUS_IGNORE);
+    MPI_File_close(&fh);
+  }
+
+  // start iterations
+  for (auto n = 1; n <= N; n++) {
+    if (me == 0) {
+      std::cout << "Iteration " << n << std::endl;
+    }
+    // fft.forward(U.data(), C1.data(), workspace.data(), heffte::scale::full);
+    fft.forward(U.data(), C1.data(), workspace.data());
+    for (auto i = 0; i < U.size(); i++) {
+      U[i] = pow(U[i], 3.0) - C * U[i];
+    };
+    // fft.forward(U.data(), C2.data(), workspace.data(), heffte::scale::full);
+    fft.forward(U.data(), C2.data(), workspace.data());
+
+    // note the order of the loops corresponding to the default order (0, 1, 2)
+    // order (0, 1, 2) means that the data in dimension 0 is contiguous
+    for (auto i = outbox.low[2]; i <= outbox.high[2]; i++) {
+      for (auto j = outbox.low[1]; j <= outbox.high[1]; j++) {
+        for (auto k = outbox.low[0]; k <= outbox.high[0]; k++) {
+          // triple indexes to linear index conversion
+          auto idx = (i - outbox.low[2]) * local_plane +
+                     (j - outbox.low[1]) * local_stride + k - outbox.low[0];
+          auto c1 = cos(2 * pi() / nx * i);
+          auto c2 = cos(2 * pi() / ny * j);
+          auto c3 = cos(2 * pi() / nz * k);
+          auto f = 3.0 / pow(h, 3.0) * (c1 + c2 + c3 - 3);
+          auto d = 1.0 - tau * f * (pow(f + 1.0, 2.0) + C - delta);
+          C1[idx] = (C1[idx] + tau * f * C2[i]) / d;
+        }
+      }
+    }
+
+    fft.backward(C1.data(), U.data(), workspace.data(), heffte::scale::full);
+  }
+
+  {
+    MPI_Datatype filetype;
+    const int gdims[] = {nx, ny, nz};
+    const auto lx = inbox.high[0] - inbox.low[0];
+    const auto ly = inbox.high[1] - inbox.low[1];
+    const auto lz = inbox.high[2] - inbox.low[2];
+    const int ldims[] = {lx, ly, lz};
+    const auto ox = inbox.low[0];
+    const auto oy = inbox.low[1];
+    const auto oz = inbox.low[2];
+    const int offset[] = {ox, oy, oz};
+    MPI_Type_create_subarray(3, gdims, ldims, offset, MPI_ORDER_C, MPI_DOUBLE,
+                             &filetype);
+    MPI_Type_commit(&filetype);
+    const unsigned int disp = 0;
+
+    MPI_File fh;
+    const std::string filename = std::to_string(N) + ".bin";
     MPI_File_open(MPI_COMM_WORLD, filename.c_str(),
                   MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
     MPI_Offset filesize = 0;
