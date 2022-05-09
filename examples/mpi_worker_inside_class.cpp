eefab78bff68c620030377ce9ba83cec0056e337
Jukka Aho <jukka.aho@vtt.fi>
2022-05-09 20:25:30 +0300

    add example mpi worker inside class

---
+++ examples/mpi_worker_inside_class.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <pfc/mpi.hpp>
+
+namespace mpi = pfc::mpi;
+
+struct mpi_worker {
+  mpi::environment env;
+  mpi::communicator comm;
+};
+
+class simulation {
+public:
+  mpi_worker mpi;
+};
+
+int main() {
+  simulation s;
+  std::cout << "I am process " << s.mpi.comm.rank() << " of "
+            << s.mpi.comm.size() << "." << std::endl;
+  return 0;
+}
