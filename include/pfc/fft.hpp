c7c76c03a3df79422aef121ed2a06f9e2ab9fc61
Jukka Aho <jukka.aho@vtt.fi>
2022-07-26 23:21:00 +0300

    FFT: be more consistent with member variable names

--- include/pfc/fft.hpp
+++ include/pfc/fft.hpp
@@ -2,31 +2,35 @@
 
 #include "constants.hpp"
 #include "decomposition.hpp"
+#include "types.hpp"
 #include <heffte.h>
 #include <iostream>
 
 namespace PFC {
 
 class FFT {
+
 private:
-  const Decomposition decomp;
+  const Decomposition m_decomp;
+  const heffte::box3d<int> m_inbox, m_outbox;
   const heffte::fft3d_r2c<heffte::backend::fftw> m_fft;
-  std::vector<std::complex<double>> wrk;
+  std::vector<std::complex<double>> m_wrk;
 
 public:
-  FFT(const std::array<int, 3> &dims, MPI_Comm comm = MPI_COMM_WORLD)
-      : decomp({dims, comm}),
-        m_fft({decomp.inbox, decomp.outbox, constants::r2c_direction, comm}),
-        wrk(std::vector<std::complex<double>>(m_fft.size_workspace())){};
+  FFT(const Vec3<int> &dims, MPI_Comm comm = MPI_COMM_WORLD)
+      : m_decomp({dims, comm}), m_inbox(m_decomp.inbox),
+        m_outbox(m_decomp.outbox),
+        m_fft({m_inbox, m_outbox, constants::r2c_direction, comm}),
+        m_wrk(std::vector<std::complex<double>>(m_fft.size_workspace())){};
 
   void forward(std::vector<double> &in,
                std::vector<std::complex<double>> &out) {
-    m_fft.forward(in.data(), out.data(), wrk.data());
+    m_fft.forward(in.data(), out.data(), m_wrk.data());
   };
 
   void backward(std::vector<std::complex<double>> &in,
                 std::vector<double> &out) {
-    m_fft.backward(in.data(), out.data(), wrk.data(), heffte::scale::full);
+    m_fft.backward(in.data(), out.data(), m_wrk.data(), heffte::scale::full);
   };
 
   size_t size_inbox() const { return m_fft.size_inbox(); }
