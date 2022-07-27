57b53b2aa1048efb06d769382c40237d1977111d
Jukka Aho <jukka.aho@vtt.fi>
2022-07-27 15:50:00 +0300

    Implement new class Time

---
+++ examples/time.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <pfc/time.hpp>
+
+using namespace std;
+using namespace pfc;
+
+void print_status(const Time &time) {
+  cout << "Simulation " << time.get_current() << "/" << time.get_t1();
+  if (time.do_save()) {
+    cout << " (writing results every " << time.get_saveat() << ")";
+  }
+  cout << endl;
+}
+
+int main() {
+  cout << std::fixed;
+  cout.precision(3);
+  Time time({0.0, 10.0, 1.0});
+  time.set_saveat(2.0);
+  cout << time << endl;
+  cout << "Starting simulation at time " << time.get_t0() << ", with timesteps "
+       << time.get_dt() << endl;
+  print_status(time);
+  while (!time.done()) {
+    time.next();
+    print_status(time);
+  }
+  return 0;
+}
---
+++ include/pfc/time.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+
+namespace pfc {
+class Time {
+private:
+  double m_t0, m_t1, m_dt;
+  int m_increments;
+  double m_saveat;
+
+public:
+  Time(const std::array<double, 3> &time)
+      : m_t0(time[0]), m_t1(time[1]), m_dt(time[2]), m_increments(0),
+        m_saveat(m_dt) {}
+
+  double get_t0() const { return m_t0; }
+  double get_t1() const { return m_t1; }
+  double get_dt() const { return m_dt; }
+  int get_increments() const { return m_increments; }
+  double get_current() const { return m_t0 + m_increments * m_dt; }
+  double get_saveat() const { return m_saveat; }
+
+  void set_saveat(double saveat) { m_saveat = saveat; }
+
+  bool done() const { return get_current() >= m_t1; }
+  void next() { m_increments += 1; }
+  bool do_save() const {
+    return (std::fmod(get_current() + 1.0e-9, m_saveat) < 1.e-6) || done() ||
+           (m_increments == 0);
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Time &t) {
+    os << "(t0 = " << t.m_t0 << ", t1 = " << t.m_t1 << ", dt = " << t.m_dt;
+    os << ", saveat = " << t.m_saveat << ", t_current = " << t.get_current()
+       << ")\n";
+    return os;
+  };
+};
+} // namespace pfc
