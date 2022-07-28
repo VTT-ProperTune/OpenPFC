c179694861592c0af79ab9c41685552021145b76
Jukka Aho <jukka.aho@vtt.fi>
2022-07-28 22:56:19 +0300

    Introduce new type alias Field = vector<double>

--- include/pfc/types.hpp
+++ include/pfc/types.hpp
@@ -1,8 +1,12 @@
 #pragma once
+
 #include <array>
+#include <vector>
 
 namespace pfc {
 
 template <class T> using Vec3 = std::array<T, 3>;
+using Field = std::vector<double>;
+// template <class T> using Field = std::vector<T>;
 
-}
+} // namespace pfc
