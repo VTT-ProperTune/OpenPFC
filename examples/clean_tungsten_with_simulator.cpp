11bd256122068b38aef4c4419544ae2d287c8875
Jukka Aho <jukka.aho@vtt.fi>
2022-07-28 11:13:54 +0300

    Implement clean tungsten with Simulator
    Tungsten model is modified so that it can be run using Simulator
    interface, i.e. Simulator<Tungsten>. Relatively small modifications
    needed to be done, and new version is examples/clean_tungsten.hpp.
    
    Based on sha256 sums of output files, new model produces the same
    results:
    
    juajukka@tohtori:/task/juajukka/tungsten$ sha256sum data1/* data2/* data3/*
    dea0ec91abf887256f679660e1116cb7e6c7e4dbd8ce071184f72059ed60f3b8 data1/u0.bin
    70a3b0b00f968d75cdaa5e8b66d8348a9697bfc3e7389374a65e36d1924c76cf data1/u1.bin
    dea0ec91abf887256f679660e1116cb7e6c7e4dbd8ce071184f72059ed60f3b8 data2/u0.bin
    70a3b0b00f968d75cdaa5e8b66d8348a9697bfc3e7389374a65e36d1924c76cf data2/u1.bin
    dea0ec91abf887256f679660e1116cb7e6c7e4dbd8ce071184f72059ed60f3b8 data3/tungsten_0000.bin
    70a3b0b00f968d75cdaa5e8b66d8348a9697bfc3e7389374a65e36d1924c76cf data3/tungsten_0001.bin

---
+++ examples/clean_tungsten_with_simulator.cpp
@@ -0,0 +1,83 @@
+#include "clean_tungsten.hpp"
+
+#include <pfc/results_writer.hpp>
+#include <pfc/simulator.hpp>
+#include <pfc/time.hpp>
+
+#include <filesystem>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+using namespace pfc;
+using namespace std;
+
+struct ui {
+  int Lx, Ly, Lz;
+  filesystem::path results_dir;
+
+  ui(int argc, char *argv[]) {
+    int me;
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
+    if (argc < 5) {
+      if (me == 0)
+        cerr << "usage: " << argv[0] << " <Lx> <Ly> <Lz> <results-dir>\n";
+      throw runtime_error("invalid command line arguments");
+    }
+
+    Lx = std::stoi(argv[1]);
+    Ly = std::stoi(argv[2]);
+    Lz = std::stoi(argv[3]);
+    results_dir = argv[4];
+    if (me == 0) {
+      if (!filesystem::exists(results_dir)) {
+        cout << "Results dir " << results_dir << " does not exist, creating\n";
+        filesystem::create_directories(results_dir);
+      }
+    }
+  }
+};
+
+void run(int Lx, int Ly, int Lz, filesystem::path results_dir) {
+
+  // make world
+  double pi = std::atan(1.0) * 4.0;
+  // 'lattice' constants for the three ordered phases that exist in 2D/3D PFC
+  // const double a1D = 2 * pi;               // stripes
+  // const double a2D = 2 * pi * 2 / sqrt(3); // triangular
+  double a3D = 2 * pi * sqrt(2); // BCC
+  double dx = a3D / 8.0;
+  double dy = a3D / 8.0;
+  double dz = a3D / 8.0;
+  double x0 = -0.5 * Lx * dx;
+  double y0 = -0.5 * Ly * dy;
+  double z0 = -0.5 * Lz * dz;
+  World world({Lx, Ly, Lz}, {x0, y0, z0}, {dx, dy, dz});
+
+  // make time
+  double t0 = 0.0;
+  double t1 = 1.0;
+  double dt = 1.0;
+  double saveat = 1.0;
+  Time time({t0, t1, dt}, saveat);
+
+  // make simulator and add results writer
+  Simulator<Tungsten> S(world, time);
+  S.add_results_writer(
+      make_unique<BinaryWriter>(results_dir / "tungsten_%04d.bin"));
+
+  // run loops
+  while (!S.done()) {
+    S.step();
+  }
+}
+
+int main(int argc, char *argv[]) {
+  cout << std::fixed;
+  cout.precision(12);
+  MPI_Init(&argc, &argv);
+  ui opts(argc, argv);
+  run(opts.Lx, opts.Ly, opts.Lz, opts.results_dir);
+  MPI_Finalize();
+  return 0;
+}
