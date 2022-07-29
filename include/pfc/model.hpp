1a35569b5b1361d72aced624a21ead0a325193b2
Jukka Aho <jukka.aho@vtt.fi>
2022-07-29 13:04:14 +0300

    Refactor Model class
    - Construct with references to other classes
    - Remove a lot of member functions no longer needed

--- include/pfc/model.hpp
+++ include/pfc/model.hpp
@@ -7,41 +7,22 @@ namespace pfc {
 class Model {
 
 private:
-  World m_world;
-  FFT m_fft;
+  World &m_world;
+  Decomposition &m_decomposition;
+  FFT &m_fft;
 
 public:
-  const int id;
-  const bool master;
+  const bool rank0;
 
-  Model(const Vec3<int> &dimensions, const Vec3<double> &origo,
-        const Vec3<double> &discretization, MPI_Comm comm = MPI_COMM_WORLD)
-      : m_world(dimensions, origo, discretization), m_fft(dimensions, comm),
-        id(m_fft.get_id()), master(id == 0) {}
+  Model(World &world, Decomposition &decomposition, FFT &fft)
+      : m_world(world), m_decomposition(decomposition), m_fft(fft),
+        rank0(m_decomposition.get_id() == 0) {}
 
-  Model(const World &world, MPI_Comm comm = MPI_COMM_WORLD)
-      : Model({world.Lx, world.Ly, world.Lz}, {world.x0, world.y0, world.z0},
-              {world.dx, world.dy, world.dz}, comm) {}
+  World &get_world() { return m_world; }
+  Decomposition &get_decomposition() { return m_decomposition; }
+  FFT &get_fft() { return m_fft; }
 
-  void fft_r2c(std::vector<double> &A, std::vector<std::complex<double>> &B) {
-    m_fft.forward(A, B);
-  }
-
-  void fft_c2r(std::vector<std::complex<double>> &A, std::vector<double> &B) {
-    m_fft.backward(A, B);
-  }
-
-  World get_world() const { return m_world; }
-  size_t size_inbox() const { return m_fft.size_inbox(); }
-  size_t size_outbox() const { return m_fft.size_outbox(); }
-  Vec3<int> get_inbox_low() const { return m_fft.get_inbox_low(); }
-  Vec3<int> get_inbox_high() const { return m_fft.get_inbox_high(); }
-  Vec3<int> get_inbox_size() const { return m_fft.get_inbox_size(); }
-  Vec3<int> get_outbox_low() const { return m_fft.get_outbox_low(); }
-  Vec3<int> get_outbox_high() const { return m_fft.get_outbox_high(); }
-  Vec3<int> get_outbox_size() const { return m_fft.get_outbox_size(); }
-
-protected:
+  /* methods that need to override for concrete implementations */
   virtual void step(double dt) = 0;
   virtual void initialize(double dt) = 0;
   virtual std::vector<double> &get_field() = 0;
