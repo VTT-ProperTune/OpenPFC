bc7a339f2c2c6532dbb4a393a4068cce18c09b23
Jukka Aho <jukka.aho@vtt.fi>
2022-08-08 19:11:40 +0300

    XdmfGen: support for new style json

--- scripts/xdmfgen.py
+++ scripts/xdmfgen.py
@@ -28,7 +28,7 @@ header = """<?xml version="1.0"?>
             </Time>
 """
 
-content = """            <Grid Name="{name}" GridType="Uniform"><Topology Reference="/Xdmf/Domain/Topology[1]" /><Geometry Reference="/Xdmf/Domain/Geometry[1]" /><Attribute Name="u" Center="Node"><DataItem Format="Binary" DataType="Float" Precision="8" Endian="Little" Dimensions="{Lz} {Ly} {Lx}">{f}</DataItem></Attribute></Grid>"""
+content = """            <Grid Name="{grid_name}" GridType="Uniform"><Topology Reference="/Xdmf/Domain/Topology[1]" /><Geometry Reference="/Xdmf/Domain/Geometry[1]" /><Attribute Name="u" Center="Node"><DataItem Format="Binary" DataType="Float" Precision="8" Endian="Little" Dimensions="{Lz} {Ly} {Lx}">{f}</DataItem></Attribute></Grid>"""
 
 footer = """
         </Grid>
@@ -42,7 +42,7 @@ def extract_number(s):
     return int(numbers[0])
 
 
-def main(args):
+def main_old(args):
     if args["json"] is not None and os.path.exists(args["json"]):
         args.update(json.load(open(args["json"])))
     if args["results_dir"] != "" and not os.path.isdir(args["results_dir"]):
@@ -61,12 +61,46 @@ def main(args):
     args["timesteps"] = " ".join("%0.3f" % (i*args["saveat"]) for i in range(len(files)))
     print(header.format(**args))
     for f in files:
-        print(content.format(**args, name=os.path.splitext(os.path.basename(f))[0], f=os.path.relpath(f)))
+        print(content.format(**args, grid_name=os.path.splitext(os.path.basename(f))[0], f=os.path.relpath(f)))
+    print(footer.format(**args))
+
+
+def main(args):
+    if args["json"] is not None and os.path.exists(args["json"]):
+        args.update(json.load(open(args["json"])))
+    files = []
+    nfiles = int((args["t1"] - args["t0"])/args["saveat"]) + 1
+    nwarnings = 0
+    for i in range(nfiles):
+        filename = args["results"] % i
+        if os.path.exists(filename):
+            files.append(filename)
+
+    expected_size = 8 * args["Lx"] * args["Ly"] * args["Lz"]
+    if os.path.getsize(files[0]) != expected_size:
+        print("File size mismatch, check lx, ly, lz!")
+        return
+    args["ntimesteps"] = len(files)
+    args["timesteps"] = " ".join("%0.3f" % (i*args["saveat"]) for i in range(len(files)))
+
+    if args["origo"] == "center":
+        args["x0"] = -0.5 * args["Lx"] * args["dx"]
+        args["y0"] = -0.5 * args["Ly"] * args["dy"]
+        args["z0"] = -0.5 * args["Lz"] * args["dz"]
+    else:
+        args["x0"] = 0.0
+        args["y0"] = 0.0
+        args["z0"] = 0.0
+
+    print(header.format(**args))
+    for f in files:
+        print(content.format(**args, grid_name=os.path.splitext(os.path.basename(f))[0], f=os.path.relpath(f)))
     print(footer.format(**args))
 
 
 def cli():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--legacy", default=False)
     parser.add_argument("--json", default="input.json", help="read settings from json file (default input.json)")
     parser.add_argument("--results_dir", default="", help="data directory (default .)")
     parser.add_argument("--Lx", type=int)
@@ -84,4 +118,8 @@ def cli():
 
 
 if __name__ == "__main__":
-    main(cli())
+    args = cli()
+    if args["legacy"]:
+        main_old(args)
+    else:
+        main(args)
