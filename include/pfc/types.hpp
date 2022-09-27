085f6153a5d088f143f1cc3680b92b8aa077164f
Jukka Aho <jukka.aho@vtt.fi>
2022-09-27 11:31:15 +0300

    Introduce new types
    Introduce new type aliases RealField, RealFieldSet, ComplexField,
    ComplexFieldSet.

--- include/pfc/types.hpp
+++ include/pfc/types.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <array>
+#include <complex>
+#include <unordered_map>
 #include <vector>
 
 namespace pfc {
 
 template <class T> using Vec3 = std::array<T, 3>;
 using Field = std::vector<double>;
+using RealField = std::vector<double>;
+using RealFieldSet = std::unordered_map<std::string, RealField &>;
+using ComplexField = std::vector<std::complex<double>>;
+using ComplexFieldSet = std::unordered_map<std::string, ComplexField &>;
+
 // template <class T> using Field = std::vector<T>;
 
 } // namespace pfc
