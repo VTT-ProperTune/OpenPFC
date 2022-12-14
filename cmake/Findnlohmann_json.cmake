80c387e885cb9db2a5a5ef3fb6bd11c4729ad668
Jukka Aho <jukka.aho@vtt.fi>
2022-12-14 17:00:23 +0200

    Add Findnlohmann_json.cmake

---
+++ cmake/Findnlohmann_json.cmake
@@ -0,0 +1,8 @@
+if(NOT nlohmann_json_FIND_VERSION)
+  set(nlohmann_json_FIND_VERSION 3.11.2)
+endif()
+set(nlohmann_json_DOWNLOAD_URL https://github.com/nlohmann/json/releases/download/v${nlohmann_json_FIND_VERSION}/json.tar.xz)
+message(STATUS "Fetching nlohmann-json from ${nlohmann_json_DOWNLOAD_URL}")
+include(FetchContent)
+FetchContent_Declare(json URL ${nlohmann_json_DOWNLOAD_URL})
+FetchContent_MakeAvailable(json)
