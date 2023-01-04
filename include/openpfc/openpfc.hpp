394bf5f9abfeca2b5f5c5a9f7da576e2088b7d9d
Jukka Aho <jukka.aho@vtt.fi>
2023-01-04 12:19:52 +0200

    Add main file openpfc.hpp

---
+++ include/openpfc/openpfc.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "constants.hpp"
+#include "decomposition.hpp"
+#include "fft.hpp"
+#include "field_modifier.hpp"
+#include "model.hpp"
+#include "mpi.hpp"
+#include "results_writer.hpp"
+#include "simulator.hpp"
+#include "time.hpp"
+#include "types.hpp"
+#include "utils.hpp"
+#include "world.hpp"
