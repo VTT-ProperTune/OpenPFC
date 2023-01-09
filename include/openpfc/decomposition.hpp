597d15ffaa8c0492c9f1f6ccef17e7172464848b
Jukka Aho <jukka.aho@vtt.fi>
2023-01-09 12:17:12 +0200

    Introduce m_world to Decomposition

--- include/openpfc/decomposition.hpp
+++ include/openpfc/decomposition.hpp
@@ -10,7 +10,8 @@ namespace pfc {
 
 class Decomposition {
 private:
-  const int Lx, Lx_c, Ly, Ly_c, Lz, Lz_c, id, tot;
+  const World &m_world;
+  const int Lx_c, Ly_c, Lz_c, id, tot;
   const heffte::box3d<int> real_indexes, complex_indexes;
   const std::array<int, 3> proc_grid;
   const std::vector<heffte::box3d<int>> real_boxes, complex_boxes;
@@ -19,10 +20,9 @@ public:
   const heffte::box3d<int> world, inbox, outbox;
   const int r2c_direction = constants::r2c_direction;
 
-  Decomposition(const std::array<int, 3> &dims, int id, int tot)
-      : Lx(dims[0]), Lx_c(floor(Lx / 2) + 1), Ly(dims[1]), Ly_c(Ly),
-        Lz(dims[2]), Lz_c(Lz), id(id), tot(tot),
-        real_indexes({0, 0, 0}, {Lx - 1, Ly - 1, Lz - 1}),
+  Decomposition(const World &world, int id, int tot)
+      : m_world(world), Lx_c(floor(m_world.Lx / 2) + 1), Ly_c(m_world.Ly),
+        Lz_c(m_world.Lz), id(id), tot(tot), real_indexes(m_world),
         complex_indexes({0, 0, 0}, {Lx_c - 1, Ly_c - 1, Lz_c - 1}),
         proc_grid(heffte::proc_setup_min_surface(real_indexes, tot)),
         real_boxes(heffte::split_world(real_indexes, proc_grid)),
@@ -34,22 +34,25 @@ public:
   };
 
   Decomposition(const World &world, MPI_Comm comm)
-      : Decomposition(world.get_size(), mpi::get_comm_rank(comm),
+      : Decomposition(world, mpi::get_comm_rank(comm),
                       mpi::get_comm_size(comm)) {}
 
   Decomposition(const Decomposition &) = delete;
   Decomposition &operator=(Decomposition &) = delete;
 
+  const World &get_world() const { return m_world; }
+
   int get_id() const { return id; }
 
   friend std::ostream &operator<<(std::ostream &os, const Decomposition &d) {
+    const World &w = d.get_world();
     os << "***** DOMAIN DECOMPOSITION STATUS *****\n";
     os << "Real-to-complex symmetry is used (r2c direction = "
        << (char)('x' + constants::r2c_direction) << ")\n";
     os << "Domain is split into " << d.tot << " parts ";
     os << "(minimum surface processor grid: [" << d.proc_grid[0] << ", "
        << d.proc_grid[1] << ", " << d.proc_grid[2] << "])\n";
-    os << "Domain in real space: [" << d.Lx << ", " << d.Ly << ", " << d.Lz
+    os << "Domain in real space: [" << w.Lx << ", " << w.Ly << ", " << w.Lz
        << "] (" << d.real_indexes.count() << " indexes)\n";
     os << "Domain in complex space: [" << d.Lx_c << ", " << d.Ly_c << ", "
        << d.Lz_c << "] (" << d.complex_indexes.count() << " indexes)\n";
