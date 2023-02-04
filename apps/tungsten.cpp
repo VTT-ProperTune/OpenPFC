cf684b1071e465a514b3bcc2beccc4b7c2c4236a
Jukka Aho <jukka.aho@vtt.fi>
2023-02-04 14:48:51 +0200

    Remove SingleSeed from tungsten.cpp

--- apps/tungsten.cpp
+++ apps/tungsten.cpp
@@ -258,60 +258,6 @@ public:
 
 }; // end of class
 
-class SingleSeed : public FieldModifier {
-
-public:
-  void apply(Model &m, double) override {
-    Params &p = dynamic_cast<Tungsten &>(m).get_params();
-    const World &w = m.get_world();
-    const Decomposition &decomp = m.get_decomposition();
-    Field &f = m.get_field();
-    Vec3<int> low = decomp.inbox.low;
-    Vec3<int> high = decomp.inbox.high;
-    auto dx = w.dx;
-    auto dy = w.dy;
-    auto dz = w.dz;
-    auto x0 = w.x0;
-    auto y0 = w.y0;
-    auto z0 = w.z0;
-
-    double s = 1.0 / sqrt(2.0);
-    std::array<double, 3> q1 = {s, s, 0};
-    std::array<double, 3> q2 = {s, 0, s};
-    std::array<double, 3> q3 = {0, s, s};
-    std::array<double, 3> q4 = {s, 0, -s};
-    std::array<double, 3> q5 = {s, -s, 0};
-    std::array<double, 3> q6 = {0, s, -s};
-    std::array<std::array<double, 3>, 6> q = {q1, q2, q3, q4, q5, q6};
-
-    long int idx = 0;
-    // double r2 = pow(0.2 * (Lx * dx), 2);
-    double r2 = pow(64.0, 2);
-    double amp_eq = p.amp_eq;
-    double u;
-    for (int k = low[2]; k <= high[2]; k++) {
-      for (int j = low[1]; j <= high[1]; j++) {
-        for (int i = low[0]; i <= high[0]; i++) {
-          double x = x0 + i * dx;
-          double y = y0 + j * dy;
-          double z = z0 + k * dz;
-          bool seedmask = x * x + y * y + z * z < r2;
-          if (!seedmask) {
-            u = p.n0;
-          } else {
-            u = p.rho_seed;
-            for (int i = 0; i < 6; i++) {
-              u += 2.0 * amp_eq * cos(q[i][0] * x + q[i][1] * y + q[i][2] * z);
-            }
-          }
-          f[idx] = u;
-          idx += 1;
-        }
-      }
-    }
-  }
-};
-
 class RandomSeeds : public FieldModifier {
 
   void apply(Model &m, double) override {
@@ -657,7 +603,13 @@ public:
     auto ic = m_settings["initial_condition"];
     if (ic["type"] == "single_seed") {
       cout << "Adding single seed initial condition" << endl;
-      m_simulator.add_initial_conditions(make_unique<SingleSeed>());
+      std::unique_ptr<SingleSeed> ic = make_unique<SingleSeed>();
+      // TODO: define amp_eq, n0, rho_seed - how?
+      Params p;
+      ic->amp_eq = p.amp_eq;
+      ic->n0 = p.n0;
+      ic->rho_seed = p.rho_seed;
+      m_simulator.add_initial_conditions(std::move(ic));
     } else if (ic["type"] == "random_seeds") {
       cout << "Adding randomized seeds initial condition" << endl;
       m_simulator.add_initial_conditions(make_unique<RandomSeeds>());
