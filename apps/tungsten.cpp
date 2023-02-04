68689201fdf8e5b6ab15bc5d8c64c660f0fa59ab
Jukka Aho <jukka.aho@vtt.fi>
2023-02-04 13:43:03 +0200

    Move FileReader and BinaryReader out from Tungsten

--- apps/tungsten.cpp
+++ apps/tungsten.cpp
@@ -467,52 +467,6 @@ public:
   }
 };
 
-class BinaryReader {
-
-private:
-  MPI_Datatype m_filetype;
-
-public:
-  void set_domain(const Vec3<int> &arr_global, const Vec3<int> &arr_local,
-                  const Vec3<int> &arr_offset) {
-    MPI_Type_create_subarray(3, arr_global.data(), arr_local.data(),
-                             arr_offset.data(), MPI_ORDER_FORTRAN, MPI_DOUBLE,
-                             &m_filetype);
-    MPI_Type_commit(&m_filetype);
-  };
-
-  MPI_Status read(const std::string &filename, Field &data) {
-    MPI_File fh;
-    MPI_Status status;
-    if (MPI_File_open(MPI_COMM_WORLD, filename.c_str(), MPI_MODE_RDONLY,
-                      MPI_INFO_NULL, &fh)) {
-      std::cout << "Unable to open file!" << std::endl;
-    }
-    MPI_File_set_view(fh, 0, MPI_DOUBLE, m_filetype, "native", MPI_INFO_NULL);
-    MPI_File_read_all(fh, data.data(), data.size(), MPI_DOUBLE, &status);
-    MPI_File_close(&fh);
-    return status;
-  }
-};
-
-class FileReader : public FieldModifier {
-
-private:
-  std::string m_filename;
-
-public:
-  explicit FileReader(const std::string &filename) : m_filename(filename) {}
-
-  void apply(Model &m, double) override {
-    const Decomposition &d = m.get_decomposition();
-    Field &f = m.get_field();
-    cout << "Reading initial condition from file" << m_filename << endl;
-    BinaryReader reader;
-    reader.set_domain(d.get_world().get_size(), d.inbox.size, d.inbox.low);
-    reader.read(m_filename, f);
-  }
-};
-
 class MovingBC : public FieldModifier {
 
 private:
@@ -561,7 +515,7 @@ public:
     for (int k = low[2]; k <= high[2]; k++)
       for (int j = low[1]; j <= high[1]; j++)
         for (int i = low[0]; i <= high[0]; i++)
-          xline[i] = max(xline[i], field[idx++]);
+          xline[i] = std::max(xline[i], field[idx++]);
 
     MPI_Reduce(xline.data(), global_xline.data(), xline.size(), MPI_DOUBLE,
                MPI_MAX, 0, comm);
