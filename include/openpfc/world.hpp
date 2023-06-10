e63cf7c5292bebb07ae3dbf7e18929a10643695a
Jukka Aho <jukka.aho@vtt.fi>
2023-06-10 10:40:22 +0300

    World: define inline getters for variables

--- include/openpfc/world.hpp
+++ include/openpfc/world.hpp
@@ -11,9 +11,15 @@ private:
   template <class T> using Vec3 = std::array<T, 3>;
 
 public:
-  const int Lx, Ly, Lz;
-  const double x0, y0, z0;
-  const double dx, dy, dz;
+  const int Lx;    ///< Length in the x-direction
+  const int Ly;    ///< Length in the y-direction
+  const int Lz;    ///< Length in the z-direction
+  const double x0; ///< Origin coordinate in the x-direction
+  const double y0; ///< Origin coordinate in the y-direction
+  const double z0; ///< Origin coordinate in the z-direction
+  const double dx; ///< Discretization parameter in the x-direction
+  const double dy; ///< Discretization parameter in the y-direction
+  const double dz; ///< Discretization parameter in the z-direction
 
   World(const Vec3<int> &dimensions, const Vec3<double> &origo,
         const Vec3<double> &discretization)
@@ -25,7 +31,65 @@ public:
       : World(dimensions, Vec3<double>{0.0, 0.0, 0.0},
               Vec3<double>{1.0, 1.0, 1.0}) {}
 
+  /**
+   * @brief Get the size of the calculation domain.
+   * @return The size of the domain: {Lx, Ly, Lz}.
+   */
   Vec3<int> get_size() const { return Vec3<int>{Lx, Ly, Lz}; }
+
+  /**
+   * @brief Get the length in the x-direction.
+   * @return The length in the x-direction.
+   */
+  int get_Lx() const { return Lx; }
+
+  /**
+   * @brief Get the length in the y-direction.
+   * @return The length in the y-direction.
+   */
+  int get_Ly() const { return Ly; }
+
+  /**
+   * @brief Get the length in the z-direction.
+   * @return The length in the z-direction.
+   */
+  int get_Lz() const { return Lz; }
+
+  /**
+   * @brief Get the origin coordinate in the x-direction.
+   * @return The origin coordinate in the x-direction.
+   */
+  double get_x0() const { return x0; }
+
+  /**
+   * @brief Get the origin coordinate in the y-direction.
+   * @return The origin coordinate in the y-direction.
+   */
+  double get_y0() const { return y0; }
+
+  /**
+   * @brief Get the origin coordinate in the z-direction.
+   * @return The origin coordinate in the z-direction.
+   */
+  double get_z0() const { return z0; }
+
+  /**
+   * @brief Get the discretization parameter in the x-direction.
+   * @return The discretization parameter in the x-direction.
+   */
+  double get_dx() const { return dx; }
+
+  /**
+   * @brief Get the discretization parameter in the y-direction.
+   * @return The discretization parameter in the y-direction.
+   */
+  double get_dy() const { return dy; }
+
+  /**
+   * @brief Get the discretization parameter in the z-direction.
+   * @return The discretization parameter in the z-direction.
+   */
+  double get_dz() const { return dz; }
 
   operator heffte::box3d<int>() const {
     return heffte::box3d<int>({0, 0, 0}, {Lx - 1, Ly - 1, Lz - 1});
