32dcf5f3806c388fd4d5adc6c11d254b4b073e9e
Jukka Aho <jukka.aho@vtt.fi>
2023-06-26 22:00:17 +0300

    Add cmake script to find Catch2

---
+++ cmake/FindCatch2.cmake
@@ -0,0 +1,15 @@
+if(NOT Catch2_FIND_VERSION)
+  set(Catch2_FIND_VERSION 3.3.2)
+endif()
+
+message(STATUS "Fetching Catch2 version ${Catch2_FIND_VERSION}")
+
+include(FetchContent)
+
+FetchContent_Declare(
+  Catch2
+  GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+  GIT_TAG        "v${Catch2_FIND_VERSION}"
+)
+
+FetchContent_MakeAvailable(Catch2)
