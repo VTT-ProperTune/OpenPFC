1107d1b5760bebcab788d25ba5f6cf8a74588322
Jukka Aho <jukka.aho@vtt.fi>
2023-06-26 17:22:49 +0300

    FileReader: implement getters and setters

--- include/openpfc/initial_conditions/file_reader.hpp
+++ include/openpfc/initial_conditions/file_reader.hpp
@@ -1,29 +1,33 @@
 #pragma once
 
+#include <iostream>
+
 #include "../binary_reader.hpp"
 #include "../field_modifier.hpp"
-
-#include <iostream>
 
 namespace pfc {
 
 class FileReader : public FieldModifier {
-
-private:
+ private:
   std::string m_filename;
 
-public:
+ public:
+  FileReader() = default;
+
+  void set_filename(std::string filename) { m_filename = filename; }
+  const std::string &get_filename() const { return m_filename; }
+
   explicit FileReader(const std::string &filename) : m_filename(filename) {}
 
   void apply(Model &m, double) override {
     const Decomposition &d = m.get_decomposition();
     Field &f = m.get_field();
-    std::cout << "Reading initial condition from file" << m_filename
+    std::cout << "Reading initial condition from file" << get_filename()
               << std::endl;
     BinaryReader reader;
     reader.set_domain(d.get_world().get_size(), d.inbox.size, d.inbox.low);
-    reader.read(m_filename, f);
+    reader.read(get_filename(), f);
   }
 };
 
-} // namespace pfc
+}  // namespace pfc
