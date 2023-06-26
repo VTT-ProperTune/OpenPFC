0af61ef8c6fa25380be5c65d217f42cdd040cd56
Jukka Aho <jukka.aho@vtt.fi>
2023-06-26 17:24:59 +0300

    Model: use pointer to FFT instead of reference

--- include/openpfc/model.hpp
+++ include/openpfc/model.hpp
@@ -1,6 +1,8 @@
 #ifndef PFC_MODEL_HPP
 #define PFC_MODEL_HPP
 
+#include <memory>
+
 #include "decomposition.hpp"
 #include "fft.hpp"
 #include "types.hpp"
@@ -26,37 +28,42 @@ namespace pfc {
  * explicitly.
  */
 class Model {
+ private:
+  FFT* m_fft = nullptr;  ///< Raw pointer to the FFT object used by the model
+  RealFieldSet m_real_fields;  ///< Collection of real-valued fields associated
+                               ///< with the model
+  ComplexFieldSet m_complex_fields;  ///< Collection of complex-valued fields
+                                     ///< associated with the model
 
-private:
-  FFT &m_fft;                       ///< Reference to the FFT object used by the model
-  RealFieldSet m_real_fields;       ///< Collection of real-valued fields associated with the model
-  ComplexFieldSet m_complex_fields; ///< Collection of complex-valued fields associated with the model
+ public:
+  bool rank0 = false;  ///< Flag indicating if the current MPI rank is 0 (useful
+                       ///< for rank-specific operations)
 
-public:
-  const bool rank0; ///< Flag indicating if the current MPI rank is 0 (useful for rank-specific operations)
+  /**
+   * @brief Construct a new Model object.
+   */
+  Model() = default;
 
   /**
    * @brief Construct a new Model object.
    *
    * @param fft Reference to the FFT object used by the model
    */
-  Model(FFT &fft) : m_fft(fft), rank0(get_decomposition().get_id() == 0) {}
+  Model(FFT &fft) : m_fft(&fft), rank0(get_decomposition().get_id() == 0) {}
 
   /**
    * @brief Return boolean flag indicating is this process rank 0.
    *
    * @return true/false
    */
-  bool is_rank0() {
-    return rank0;
-  }
+  bool is_rank0() { return rank0; }
 
   /**
    * @brief Get the decomposition object associated with the model.
    *
    * @return Reference to the Decomposition object
    */
-  const Decomposition &get_decomposition() { return m_fft.get_decomposition(); }
+  const Decomposition &get_decomposition() { return get_fft().get_decomposition(); }
 
   /**
    * @brief Get the world object associated with the model.
@@ -66,13 +73,26 @@ public:
   const World &get_world() { return get_decomposition().get_world(); }
 
   /**
+   * @brief Set the fft object
+   * 
+   * @param fft 
+   */
+  void set_fft(FFT& fft) {
+    m_fft = &fft;
+    rank0 = (get_decomposition().get_id() == 0);
+  }
+
+  /**
    * @brief Get the FFT object associated with the model.
    *
    * @return Reference to the FFT object
    */
   FFT &get_fft() {
-    m_fft.reset_fft_time();
-    return m_fft;
+    if (m_fft == nullptr) {
+      throw std::runtime_error("FFT object has not been set.");
+    }
+    m_fft->reset_fft_time();
+    return *m_fft;
   }
 
   /**
@@ -103,7 +123,9 @@ public:
    * @param field_name Name of the field to check
    * @return True if the field exists, False otherwise
    */
-  bool has_real_field(const std::string &field_name) { return m_real_fields.count(field_name) > 0; }
+  bool has_real_field(const std::string &field_name) {
+    return m_real_fields.count(field_name) > 0;
+  }
 
   /**
    * @brief Add a real-valued field to the model.
@@ -111,7 +133,9 @@ public:
    * @param name Name of the field
    * @param field Reference to the RealField object representing the field
    */
-  void add_real_field(const std::string &name, RealField &field) { m_real_fields.insert({name, field}); }
+  void add_real_field(const std::string &name, RealField &field) {
+    m_real_fields.insert({name, field});
+  }
 
   /**
    * @brief Check if the model has a complex-valued field with the given name.
@@ -119,7 +143,9 @@ public:
    * @param field_name Name of the field to check
    * @return True if the field exists, False otherwise
    */
-  bool has_complex_field(const std::string &field_name) { return m_complex_fields.count(field_name) > 0; }
+  bool has_complex_field(const std::string &field_name) {
+    return m_complex_fields.count(field_name) > 0;
+  }
 
   /**
    * @brief Add a complex-valued field to the model.
@@ -127,7 +153,9 @@ public:
    * @param name Name of the field
    * @param field Reference to the ComplexField object representing the field
    */
-  void add_complex_field(const std::string &name, ComplexField &field) { m_complex_fields.insert({name, field}); }
+  void add_complex_field(const std::string &name, ComplexField &field) {
+    m_complex_fields.insert({name, field});
+  }
 
   /**
    * @brief Get a reference to the real-valued field with the given name.
@@ -135,7 +163,9 @@ public:
    * @param name Name of the field
    * @return Reference to the RealField object
    */
-  RealField &get_real_field(const std::string &name) { return m_real_fields.find(name)->second; }
+  RealField &get_real_field(const std::string &name) {
+    return m_real_fields.find(name)->second;
+  }
 
   /**
    * @brief Get a reference to the complex-valued field with the given name.
@@ -143,15 +173,20 @@ public:
    * @param name Name of the field
    * @return Reference to the ComplexField object
    */
-  ComplexField &get_complex_field(const std::string &name) { return m_complex_fields.find(name)->second; }
+  ComplexField &get_complex_field(const std::string &name) {
+    return m_complex_fields.find(name)->second;
+  }
 
   /**
-   * @brief Check if the model has a field with the given name (real or complex).
+   * @brief Check if the model has a field with the given name (real or
+   * complex).
    *
    * @param field_name Name of the field to check
    * @return True if the field exists, False otherwise
    */
-  bool has_field(const std::string &field_name) { return has_real_field(field_name) || has_complex_field(field_name); }
+  bool has_field(const std::string &field_name) {
+    return has_real_field(field_name) || has_complex_field(field_name);
+  }
 
   /**
    * @brief Get a reference to the default primary unknown field.
@@ -162,6 +197,6 @@ public:
    */
   virtual Field &get_field() { return get_real_field("default"); };
 };
-} // namespace pfc
+}  // namespace pfc
 
 #endif
