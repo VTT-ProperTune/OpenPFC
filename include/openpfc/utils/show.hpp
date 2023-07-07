57fab44464887530a26313926a2b7dcb73878a28
Jukka Aho <jukka.aho@vtt.fi>
2023-07-07 10:16:23 +0300

    Implement utils::show to display matrices and nd arrays

---
+++ include/openpfc/utils/show.hpp
@@ -0,0 +1,45 @@
+#ifndef PFC_UTILS_SHOW
+#define PFC_UTILS_SHOW
+
+#include "typename.hpp"
+#include <array>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+namespace pfc {
+namespace utils {
+
+template <typename T>
+void show(const std::vector<T> &data, const std::array<int, 3> &size, const std::array<int, 3> &offsets) {
+  std::cout << size[0] << "×" << size[1] << "×" << size[2] << " Array<3, " << pfc::TypeName<T>::get()
+            << ">:" << std::endl;
+  for (int k = 0; k < size[2]; ++k) {
+    std::cout << "[:, :, " << offsets[2] + k << "] = " << std::endl;
+    for (int i = 0; i < size[0]; ++i) {
+      for (int j = 0; j < size[1]; ++j) {
+        int idx = k * size[0] * size[1] + j * size[0] + i;
+        std::cout << std::setw(9) << std::setprecision(6) << std::fixed << data[idx] << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+}
+template <typename T>
+void show(const std::vector<T> &data, const std::array<int, 2> &size, const std::array<int, 2> &offsets) {
+  std::cout << size[0] << "×" << size[1] << " Array<2, " << pfc::TypeName<T>::get() << ">:" << std::endl;
+  for (int i = 0; i < size[0]; ++i) {
+    for (int j = 0; j < size[1]; ++j) {
+      size_t idx = j * size[0] + i;
+      std::cout << std::setw(9) << std::setprecision(6) << std::fixed << data[idx] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+} // namespace utils
+} // namespace pfc
+
+#endif
