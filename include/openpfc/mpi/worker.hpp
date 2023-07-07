4f3c46b32784740c7f6ed725a87c018fc1aca54c
Jukka Aho <jukka.aho@vtt.fi>
2023-07-07 10:17:22 +0300

    MPI_Worker: use MPI_COMM_WORLD as default communicator

--- include/openpfc/mpi/worker.hpp
+++ include/openpfc/mpi/worker.hpp
@@ -14,11 +14,11 @@ namespace pfc {
  * rank and number of processes in the MPI communicator.
  */
 class MPI_Worker {
-  MPI_Comm m_comm;  ///< MPI communicator for this worker
-  int m_rank;       ///< Rank of this worker process in the MPI communicator
-  int m_num_procs;  ///< Number of processes in the MPI communicator
+  MPI_Comm m_comm; ///< MPI communicator for this worker
+  int m_rank;      ///< Rank of this worker process in the MPI communicator
+  int m_num_procs; ///< Number of processes in the MPI communicator
 
- public:
+public:
   /**
    * @brief Constructs an MPI worker instance and initializes MPI.
    *
@@ -30,15 +30,14 @@ class MPI_Worker {
    * @param argv Pointer to an array of command-line arguments
    * @param comm MPI communicator to use
    */
-  MPI_Worker(int argc, char *argv[], MPI_Comm comm) : m_comm(comm) {
+  MPI_Worker(int argc, char *argv[], MPI_Comm comm = MPI_COMM_WORLD) : m_comm(comm) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(m_comm, &m_rank);
     MPI_Comm_size(m_comm, &m_num_procs);
     if (m_rank != 0) {
       mute();
     }
-    std::cout << "MPI_Init(): initialized " << m_num_procs << " processes"
-              << std::endl;
+    std::cout << "MPI_Init(): initialized " << m_num_procs << " processes" << std::endl;
   }
   /**
    * @brief Destroys the MPI worker instance and finalizes MPI.
@@ -78,4 +77,4 @@ class MPI_Worker {
   void unmute() { std::cout.clear(); }
 };
 
-}  // namespace pfc
+} // namespace pfc
