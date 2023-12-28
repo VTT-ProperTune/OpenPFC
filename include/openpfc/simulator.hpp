ab8849cc3c3caeaaee13a45176cb15ebfa47ae60
Jukka Aho <jukka.aho@vtt.fi>
2023-12-28 13:53:10 +0200

    Implement getters get_initial_conditions and get_boundary_conditions

--- include/openpfc/simulator.hpp
+++ include/openpfc/simulator.hpp
@@ -63,6 +63,22 @@ public:
     return add_results_writer("default", std::move(writer));
   }
 
+  /**
+   * @brief Adds initial conditions to the simulation.
+   *
+   * This function takes a unique pointer to a FieldModifier object, which is
+   * used to modify the initial conditions of a field in the model. The field to
+   * be modified is determined by the `get_field_name` method of the
+   * FieldModifier object. If the field name is "default", a warning message is
+   * printed. If the model has the field, the FieldModifier is added to the
+   * initial conditions and the function returns true. If the model does not
+   * have the field, a warning message is printed and the function returns
+   * false.
+   *
+   * @param modifier A unique pointer to a FieldModifier object.
+   * @return True if the FieldModifier was successfully added to the initial
+   * conditions, false otherwise.
+   */
   bool add_initial_conditions(std::unique_ptr<FieldModifier> modifier) {
     Model &model = get_model();
     std::string field_name = modifier->get_field_name();
@@ -79,6 +95,34 @@ public:
     }
   }
 
+  /**
+   * @brief Gets the initial conditions of the simulation.
+   *
+   * This function returns a const reference to the vector of unique pointers to
+   * FieldModifier objects that represent the initial conditions of the
+   * simulation.
+   *
+   * @return A const reference to the vector of unique pointers to FieldModifier
+   * objects.
+   */
+  const std::vector<std::unique_ptr<FieldModifier>> &get_initial_conditions() const { return m_initial_conditions; }
+
+  /**
+   * @brief Adds boundary conditions to the simulation.
+   *
+   * This function takes a unique pointer to a FieldModifier object, which is
+   * used to modify the boundary conditions of a field in the model. The field
+   * to be modified is determined by the `get_field_name` method of the
+   * FieldModifier object. If the field name is "default", a warning message is
+   * printed. If the model has the field, the FieldModifier is added to the
+   * boundary conditions and the function returns true. If the model does not
+   * have the field, a warning message is printed and the function returns
+   * false.
+   *
+   * @param modifier A unique pointer to a FieldModifier object.
+   * @return True if the FieldModifier was successfully added to the boundary
+   * conditions, false otherwise.
+   */
   bool add_boundary_conditions(std::unique_ptr<FieldModifier> modifier) {
     Model &model = get_model();
     std::string field_name = modifier->get_field_name();
@@ -94,6 +138,18 @@ public:
       return false;
     }
   }
+
+  /**
+   * @brief Gets the boundary conditions of the simulation.
+   *
+   * This function returns a const reference to the vector of unique pointers to
+   * FieldModifier objects that represent the boundary conditions of the
+   * simulation.
+   *
+   * @return A const reference to the vector of unique pointers to FieldModifier
+   * objects.
+   */
+  const std::vector<std::unique_ptr<FieldModifier>> &get_boundary_conditions() const { return m_boundary_conditions; }
 
   void set_result_counter(int result_counter) { m_result_counter = result_counter; }
 
