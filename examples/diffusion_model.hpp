dc8a6ffab1b31eed1c98ba9cdf1d93f5daabcfd0
Jukka Aho <jukka.aho@vtt.fi>
2025-04-28 14:01:30 +0300

    Refactor Diffusion model to use World API for origin and spacing retrieval

--- examples/diffusion_model.hpp
+++ examples/diffusion_model.hpp
@@ -33,14 +33,17 @@ public:
     Vec3<int> o_low = decomp.outbox.low;
     Vec3<int> o_high = decomp.outbox.high;
 
+    auto origin = w.origin();
+    auto spacing = w.spacing();
+
     int idx = 0;
     double D = 1.0;
     for (int k = i_low[2]; k <= i_high[2]; k++) {
       for (int j = i_low[1]; j <= i_high[1]; j++) {
         for (int i = i_low[0]; i <= i_high[0]; i++) {
-          double x = w.x0 + i * w.dx;
-          double y = w.y0 + j * w.dy;
-          double z = w.z0 + k * w.dz;
+          double x = origin[0] + i * spacing[0];
+          double y = origin[1] + j * spacing[1];
+          double z = origin[2] + k * spacing[2];
           psi[idx] = exp(-(x * x + y * y + z * z) / (4.0 * D));
           if (abs(x) < 1.0e-9 && abs(y) < 1.0e-9 && abs(z) < 1.0e-9) {
             cout << "Found midpoint from index " << idx << endl;
@@ -53,15 +56,15 @@ public:
 
     idx = 0;
     const double pi = std::atan(1.0) * 4.0;
-    const double fx = 2.0 * pi / (w.dx * w.Lx);
-    const double fy = 2.0 * pi / (w.dy * w.Ly);
-    const double fz = 2.0 * pi / (w.dz * w.Lz);
+    const double fx = 2.0 * pi / (spacing[0] * w.size()[0]);
+    const double fy = 2.0 * pi / (spacing[1] * w.size()[1]);
+    const double fz = 2.0 * pi / (spacing[2] * w.size()[2]);
     for (int k = o_low[2]; k <= o_high[2]; k++) {
       for (int j = o_low[1]; j <= o_high[1]; j++) {
         for (int i = o_low[0]; i <= o_high[0]; i++) {
-          const double ki = (i <= w.Lx / 2) ? i * fx : (i - w.Lx) * fx;
-          const double kj = (j <= w.Ly / 2) ? j * fy : (j - w.Ly) * fy;
-          const double kk = (k <= w.Lz / 2) ? k * fz : (k - w.Lz) * fz;
+          const double ki = (i <= w.size()[0] / 2) ? i * fx : (i - w.size()[0]) * fx;
+          const double kj = (j <= w.size()[1] / 2) ? j * fy : (j - w.size()[1]) * fy;
+          const double kk = (k <= w.size()[2] / 2) ? k * fz : (k - w.size()[2]) * fz;
           const double kLap = -(ki * ki + kj * kj + kk * kk);
           opL[idx++] = 1.0 / (1.0 - dt * kLap);
         }
