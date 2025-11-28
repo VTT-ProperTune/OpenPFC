6bfc86dfccacf24f134be7da8506ae196c27a98a
Jukka Aho <ahojukka5@gmail.com>
2025-11-28 22:02:37 +0200

    fix(ui): Update include path for ui_errors header
    - Change include from openpfc/ui_errors.hpp to openpfc/ui/errors.hpp
    - Fixes include path after header reorganization

--- src/openpfc/ui_errors.cpp
+++ src/openpfc/ui_errors.cpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 VTT Technical Research Centre of Finland Ltd
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#include "openpfc/ui_errors.hpp"
+#include "openpfc/ui/errors.hpp"
 
 namespace pfc {
 namespace ui {
