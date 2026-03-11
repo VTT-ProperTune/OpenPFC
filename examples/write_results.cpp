4512ca73589a080e475148406e19ac0c58526627
Jukka Aho <jukka.aho@vtt.fi>
2026-03-11 20:49:31 +0200

    chore(examples): Add binary_writer include to write_results example
    - Include openpfc/frontend/io/binary_writer.hpp for results writer

--- examples/write_results.cpp
+++ examples/write_results.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 VTT Technical Research Centre of Finland Ltd
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+#include <openpfc/frontend/io/binary_writer.hpp>
 #include <openpfc/kernel/simulation/results_writer.hpp>
 
 #include <fstream>
