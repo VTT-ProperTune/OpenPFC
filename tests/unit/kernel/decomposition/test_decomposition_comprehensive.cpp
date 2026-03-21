80b73f3ba64e8cfa4e3a2d361ec1e75323643302
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 11:52:10 +0200

    test_decomposition_comprehensive: angle-bracket world and decomposition includes

--- tests/unit/kernel/decomposition/test_decomposition_comprehensive.cpp
+++ tests/unit/kernel/decomposition/test_decomposition_comprehensive.cpp
@@ -3,9 +3,9 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-#include "openpfc/kernel/data/world.hpp"
-#include "openpfc/kernel/decomposition/decomposition.hpp"
-#include "openpfc/kernel/decomposition/decomposition_factory.hpp"
+#include <openpfc/kernel/data/world.hpp>
+#include <openpfc/kernel/decomposition/decomposition.hpp>
+#include <openpfc/kernel/decomposition/decomposition_factory.hpp>
 
 using namespace pfc;
 
