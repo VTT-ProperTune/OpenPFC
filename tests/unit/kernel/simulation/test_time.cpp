27f9b050eaea8b21e2afbc5f0d92a0c6ddf23042
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 11:52:56 +0200

    test_time: include time.hpp as <openpfc/kernel/simulation/time.hpp>

--- tests/unit/kernel/simulation/test_time.cpp
+++ tests/unit/kernel/simulation/test_time.cpp
@@ -4,7 +4,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include "openpfc/kernel/simulation/time.hpp"
+#include <openpfc/kernel/simulation/time.hpp>
 
 using namespace Catch::Matchers;
 using namespace pfc;
