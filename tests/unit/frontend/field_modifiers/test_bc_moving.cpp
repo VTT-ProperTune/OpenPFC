2d31943b44bb315e0d15f2bfe565003593c5f90e
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 11:49:58 +0200

    test_bc_moving: switch OpenPFC includes from quotes to <openpfc/...>

--- tests/unit/frontend/field_modifiers/test_bc_moving.cpp
+++ tests/unit/frontend/field_modifiers/test_bc_moving.cpp
@@ -8,13 +8,13 @@
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-#include "openpfc/kernel/data/world.hpp"
-#include "openpfc/kernel/data/world_types.hpp"
-#include "openpfc/kernel/decomposition/decomposition.hpp"
-#include "openpfc/kernel/decomposition/decomposition_factory.hpp"
-#include "openpfc/kernel/fft/fft.hpp"
-#include "openpfc/kernel/simulation/boundary_conditions/moving_bc.hpp"
-#include "openpfc/kernel/simulation/model.hpp"
+#include <openpfc/kernel/data/world.hpp>
+#include <openpfc/kernel/data/world_types.hpp>
+#include <openpfc/kernel/decomposition/decomposition.hpp>
+#include <openpfc/kernel/decomposition/decomposition_factory.hpp>
+#include <openpfc/kernel/fft/fft.hpp>
+#include <openpfc/kernel/simulation/boundary_conditions/moving_bc.hpp>
+#include <openpfc/kernel/simulation/model.hpp>
 
 using namespace pfc;
 using Catch::Approx;
