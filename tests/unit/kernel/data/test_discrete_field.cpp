15614c0198f6834341dfd1d071dd31653b3c2593
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 11:51:49 +0200

    test_discrete_field: include discrete_field.hpp as <openpfc/kernel/data/discrete_field.hpp>

--- tests/unit/kernel/data/test_discrete_field.cpp
+++ tests/unit/kernel/data/test_discrete_field.cpp
@@ -6,7 +6,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include "openpfc/kernel/data/discrete_field.hpp"
+#include <openpfc/kernel/data/discrete_field.hpp>
 
 using namespace Catch::Matchers;
 using namespace pfc;
