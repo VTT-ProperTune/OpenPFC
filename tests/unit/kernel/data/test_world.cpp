e1311756ed826a277c5276382de476c095db2423
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 11:52:01 +0200

    test_world: angle-bracket includes for strong_types.hpp and world.hpp

--- tests/unit/kernel/data/test_world.cpp
+++ tests/unit/kernel/data/test_world.cpp
@@ -6,8 +6,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include "openpfc/kernel/data/strong_types.hpp"
-#include "openpfc/kernel/data/world.hpp"
+#include <openpfc/kernel/data/strong_types.hpp>
+#include <openpfc/kernel/data/world.hpp>
 
 using namespace Catch::Matchers;
 using namespace pfc;
