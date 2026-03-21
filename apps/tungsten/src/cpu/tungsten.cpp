bcad4392119cf76c996a570fd5031f1504b97de8
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 10:19:02 +0200

    refactor(tungsten): qualify App and std::cout in CPU main

--- apps/tungsten/src/cpu/tungsten.cpp
+++ apps/tungsten/src/cpu/tungsten.cpp
@@ -1,11 +1,12 @@
 // SPDX-FileCopyrightText: 2025 VTT Technical Research Centre of Finland Ltd
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+#include <iostream>
 #include <tungsten/cpu/tungsten.hpp>
 
 int main(int argc, char *argv[]) {
-  cout << std::fixed;
-  cout.precision(3);
-  App<Tungsten> app(argc, argv);
+  std::cout << std::fixed;
+  std::cout.precision(3);
+  pfc::ui::App<Tungsten> app(argc, argv);
   return app.main();
 }
