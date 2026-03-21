4eaf24fba858636b96c512f05793fc5a568b4e56
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 10:35:39 +0200

    decomposition_factory.cpp: include <mpi.h> first; decomposition headers as <openpfc/...> (factory after decomposition.hpp)

--- src/openpfc/kernel/decomposition/decomposition_factory.cpp
+++ src/openpfc/kernel/decomposition/decomposition_factory.cpp
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: 2025 VTT Technical Research Centre of Finland Ltd
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#include "openpfc/kernel/decomposition/decomposition_factory.hpp"
-#include "openpfc/kernel/decomposition/decomposition.hpp"
 #include <mpi.h>
+#include <openpfc/kernel/decomposition/decomposition.hpp>
+#include <openpfc/kernel/decomposition/decomposition_factory.hpp>
 
 namespace pfc {
 
