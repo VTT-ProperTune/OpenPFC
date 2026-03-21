1c58eda9e7106dd92f034654aa4eef790658d609
Jukka Aho <ahojukka5@gmail.com>
2026-03-21 10:17:43 +0200

    refactor(tungsten): drop using-directives in cuda tungsten.hpp
    - Remove using namespace pfc::ui and std; add iostream
    - Qualify Simulator and use std::cout for TUNGSTEN_DEBUG in CUDA step()

--- apps/tungsten/include/tungsten/cuda/tungsten.hpp
+++ apps/tungsten/include/tungsten/cuda/tungsten.hpp
@@ -11,10 +11,8 @@
 
 #include <openpfc/frontend/ui/ui.hpp>
 #include <tungsten/common/tungsten_input.hpp>
+#include <iostream>
 #include <tungsten/cuda/tungsten_model.hpp>
-
-using namespace pfc::ui;
-using namespace std;
 
 /*
 Sometimes, one need to get access to the simulator during stepping. This can be
@@ -22,9 +20,11 @@ done by overriding the following function. The default implementation just runs
 m.step(t) so if there's no need to access the simulator, it's not necessary to
 override this function.
 */
-template <typename RealType> void step(Simulator &s, TungstenCUDA<RealType> &m) {
+template <typename RealType>
+void step(pfc::Simulator &s, TungstenCUDA<RealType> &m) {
 #ifdef TUNGSTEN_DEBUG
-  if (m.is_rank0()) cout << "Performing Tungsten CUDA step" << endl;
+  if (m.is_rank0())
+    std::cout << "Performing Tungsten CUDA step" << std::endl;
 #endif
   double t = s.get_time().get_current();
   m.step(t);
